b6fb81bd53590ce8da34093bd6e47f21dd50b5eb
yassir <yassirmelendezaaviles14@gmail.com>
2026-05-22 18:06:17 -0300

    v2

--- config/cors.php
+++ config/cors.php
@@ -7,9 +7,13 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://127.0.0.1:3000',
         'http://localhost:5173',
+        'http://127.0.0.1:5173',
         'http://localhost:8080',
+        'http://127.0.0.1:8080',
         'http://localhost:8082',
+        'http://127.0.0.1:8082',
     ],
 
     'allowed_origins_patterns' => [],
